e23538013fb10be246af3d7fe92cf3e0c69638f7
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-11 12:46:47 -0400

    criado o http setup do serviço

---
+++ Services/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IxcNet.Services
+{
+    public partial class IxcNetService
+    {
+        HttpClient _http = new();
+
+        public void Setup(string host, string token)
+        {
+            _http.BaseAddress = new Uri(host);
+            _http.DefaultRequestHeaders.Add("ixcsoft", "listar");
+            _http.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(token)));
+        }
+    }
+}
