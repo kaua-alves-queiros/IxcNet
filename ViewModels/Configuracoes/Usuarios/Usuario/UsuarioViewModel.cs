6421a1c91d2a4edf8e6d6e73e543cdad9a1e0068
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-11 23:00:20 -0400

    criado view model do usuario

---
+++ ViewModels/Configuracoes/Usuarios/Usuario/UsuarioViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace OneClick.ViewModels.Ixc
+{
+    public class UsuarioViewModel
+    {
+        public string? pagamentos_dia_atual { get; set; }
+        public string? enviar_monitoramento_host { get; set; }
+        public string? tipo_alcada { get; set; }
+        public string? enviar_notificacao_backup { get; set; }
+        public string? inmap_filtra_vendedor { get; set; }
+        public string? imagem { get; set; }
+        public string? filtra_departamento_ticket { get; set; }
+        public string? filtra_funcionario_ticket { get; set; }
+        public string? mostrar_ticket_sem_funcionario { get; set; }
+        public string? id { get; set; }
+        public string? id_grupo { get; set; }
+        public string? nome { get; set; }
+        public string? senha { get; set; }
+        public string? email { get; set; }
+        public string? status { get; set; }
+        public string? caixa_fn_receber { get; set; }
+        public string? id_caixa { get; set; }
+        public string? recebimentos_dia_atual { get; set; }
+        public string? lancamentos_dia_atual { get; set; }
+        public string? vendedor_padrao { get; set; }
+        public string? funcionario { get; set; }
+        public string? filtra_setor { get; set; }
+        public string? filtra_funcionario { get; set; }
+        public string? desc_max_recebimento { get; set; }
+        public string? desc_max_venda { get; set; }
+        public string? crm_filtra_vendedor { get; set; }
+        public string? acesso_webservice { get; set; }
+        public string? mostrar_os_sem_funcionario { get; set; }
+        public string? callcenter { get; set; }
+        public string? user_callcenter { get; set; }
+        public string? desc_max_renegociacao { get; set; }
+        public string? permite_inutilizar_patrimonio { get; set; }
+        public string? alter_passwd_date { get; set; }
+        public string? permite_acesso_ixc_mobile { get; set; }
+        public string? language { get; set; }
+        public string? permite_ver_diferenca { get; set; }
+        public string? template { get; set; }
+        public string? administrador_kanban { get; set; }
+    }
+}
