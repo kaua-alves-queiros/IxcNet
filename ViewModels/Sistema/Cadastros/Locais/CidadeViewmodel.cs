9e9e7a89c9cedb1575c112fbbba1adbc54846cdc
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-11 23:02:32 -0400

    criado view da cidade

---
+++ ViewModels/Sistema/Cadastros/Locais/CidadeViewmodel.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IxcNet.ViewModels.Sistema.Cadastros.Locais
+{
+    public class CidadeViewmodel
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? id { get; set; }
+        public string? origem { get; set; }
+        public string? nome { get; set; }
+        public string? uf { get; set; }
+        public string? regiao { get; set; }
+        public string? cod_ibge { get; set; }
+        public string? cod_siafi { get; set; }
+        public string? cod_cidade_nfse_forquilhinha_sc { get; set; }
+    }
+}
