62bff7538d5d0e484c2c31ac1e089cfea9d84656
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-12 12:37:39 -0400

    implementado List de Cliente

---
+++ IxcNet/Services/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using IcNet.ViewModels;
+using IxcNet.ViewModels.Sistema.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IxcNet.Services
+{
+    partial class IxcNetService
+    {
+        public async Task<List<ClienteViewModel>?> ClienteListlAsync(QueryBuilder? query = null)
+        {
+            try
+            {
+                query ??= QueryBuilder.List();
+
+                var route = "cliente";
+                query.ModelName = route;
+
+                var response = await _http.PostAsync(route, query.GetContent());
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<IxcResponseViewModel<ClienteViewModel>>(content)!.registros!;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+    }
+}
