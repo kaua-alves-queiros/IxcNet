9c618968788dbc7c3088cbeb467f8357fd5c5916
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-12 16:06:34 -0400

    criada interface para modelos nomeados

---
+++ IxcNet/Interfaces/InamedModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IxcNet.Interfaces
+{
+    public interface INamedModel
+    {
+        public string? ModelName { get; }
+    }
+}
