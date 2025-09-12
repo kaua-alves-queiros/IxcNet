9a1fdd25d3cde27b0b9df80860a9d0211f5ec75d
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-11 23:04:01 -0400

    criado view de assunto de os

---
+++ ViewModels/Sistema/Suporte/Cadastro/SuOssAssuntoViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IxcNet.ViewModels.Sistema.Suporte.Cadastro
+{
+    public class SuOssAssuntoViewModel
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? id { get; set; }
+        public string? imprimir_prod_serv { get; set; }
+        public string? modelo_email { get; set; }
+        public string? wiz_comodato { get; set; }
+        public string? wiz_produtos { get; set; }
+        public string? wiz_mensalidade { get; set; }
+        public string? wiz_arquivos { get; set; }
+        public string? wiz_localizacao { get; set; }
+        public string? wiz_autorizar_ONU { get; set; }
+        public string? wiz_resumo_os { get; set; }
+        public string? localizacao_obrigatoria_cliente_finalizacao_os { get; set; }
+        public string? localizacao_obrigatoria_login_finalizacao_os { get; set; }
+        public string? obrigar_processo_atendimento { get; set; }
+        public string? id_processo { get; set; }
+        public string? login_obrigatorio { get; set; }
+        public string? id_resposta_padrao_finalizacao { get; set; }
+        public string? mostrar_no_service { get; set; }
+        public string? id_sms_deslocamento { get; set; }
+        public string? considerar_sla { get; set; }
+        public string? id_msg_omnichannel_deslocamento { get; set; }
+        public string? ativo { get; set; }
+        public string? assunto { get; set; }
+        public string? su_oss_modelo_impressao { get; set; }
+        public string? tipo { get; set; }
+        public string? meta_horas_abertura { get; set; }
+        public string? tipo_comissao { get; set; }
+        public string? tipo_cobranca { get; set; }
+        public string? valor_comissao { get; set; }
+        public string? id_oss_kit { get; set; }
+        public string? cor_marcador { get; set; }
+        public string? layout_impressao { get; set; }
+        public string? id_tipo_doc_pedido { get; set; }
+        public string? id_tipo_doc_servico { get; set; }
+        public string? id_tipo_doc_comodato { get; set; }
+        public string? finalidade { get; set; }
+        public string? mostra_hotsite { get; set; }
+        public string? id_resposta_padrao { get; set; }
+        public string? formato_endereco { get; set; }
+        public string? permite_abrir_cliente_atraso { get; set; }
+        public string? endereco_padrao { get; set; }
+        public string? numero_de_vias { get; set; }
+        public string? horario_tempo_assunto { get; set; }
+        public string? fat_somente_finalizada { get; set; }
+        public string? meta_horas_agendamento { get; set; }
+        public string? id_cond_pag_servico { get; set; }
+        public string? id_cond_pag_produto { get; set; }
+        public string? sla_apenas_dias_uteis { get; set; }
+        public string? metas_horas_abertura_ticket { get; set; }
+        public string? quantidade_fotos_finalizacao_os { get; set; }
+        public string? exige_fotos_finalizacao_os { get; set; }
+        public string? diagnostico_obrigatorio_finalizacao_os { get; set; }
+        public string? wiz_servico { get; set; }
+        public string? card_data_reservada { get; set; }
+        public string? id_cond_pag_patrimonio_venda { get; set; }
+        public string? id_tipo_doc_patrimonio_venda { get; set; }
+        public string? validar_choque_horarios_agendamento_os { get; set; }
+        public string? ultima_atualizacao { get; set; }
+        public string? msg_regiao_manutencao { get; set; }
+        public string? id_vendedor_faturamento { get; set; }
+        public string? setor_su_oss_chamado { get; set; }
+        public string? descricao { get; set; }
+        public string? equipe_obrigatoria_finalizacao_os { get; set; }
+        public string? habilitar_mini_projeto { get; set; }
+        public string? wiz_assinatura_obrig { get; set; }
+        public string? mesclar_mini_projetos_ao_finalizar_os { get; set; }
+        public string? id_checklist { get; set; }
+        public string? wiz_service_mobile_adicionais { get; set; }
+        public string? wiz_service_mobile_onu { get; set; }
+        public string? wiz_service_mobile_loc { get; set; }
+        public string? wiz_service_mobile_anexos { get; set; }
+        public string? wiz_service_mobile_checklist { get; set; }
+        public string? wiz_service_mobile_enviar_sms_deslocamento { get; set; }
+        public string? service_mobile_max_parc_adic_serv { get; set; }
+        public string? integracao_assinatura_digital { get; set; }
+        public string? wiz_service_mobile_config_dispositivo { get; set; }
+        public string? contrato_obrigatorio { get; set; }
+        public string? quantidade_equipamentos { get; set; }
+        public string? exige_comodato_finalizar_os { get; set; }
+        public string? exige_produto_finalizar_os { get; set; }
+        public string? quantidade_produtos { get; set; }
+    }
+}
