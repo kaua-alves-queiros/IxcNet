edc35deb7f06e2f1fe6413ab0a351452dc1c4f03
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-11 23:03:14 -0400

    criado view de cto

---
+++ ViewModels/Sistema/InMap/Elementos/CaixaDeAtendimento/CaixaViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IxcNet.ViewModels.Sistema.InMap.Elementos.CaixaDeAtendimento
+{
+    public class CaixaViewModel
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? id { get; set; }
+        public string? descricao { get; set; }
+        public string? tipo { get; set; }
+        public string? id_projeto { get; set; }
+        public string? id_transmissor { get; set; }
+        public string? id_interface { get; set; }
+        public string? id_tecnologia { get; set; }
+        public string? capacidade { get; set; }
+        public string? latitude { get; set; }
+        public string? longitude { get; set; }
+        public string? codigo_estilo_caixa { get; set; }
+        public string? obs_caixa_ftth { get; set; }
+        public string? status { get; set; }
+        public string? idx { get; set; }
+        public string? ultima_atualizacao { get; set; }
+        public string? cep { get; set; }
+        public string? endereco { get; set; }
+        public string? numero { get; set; }
+        public string? bairro { get; set; }
+        public string? id_cidade { get; set; }
+    }
+}
