6104483216c6471c6bf299d5eb7d6f1df3e4e4fb
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-12 12:37:16 -0400

    criado query builder

---
+++ IxcNet/Services/QueryBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+
+namespace IxcNet.Services
+{
+    public class QueryBuilder
+    {
+        public string? ModelName { get; set; }
+        public string? FieldName { get; set; }
+        public string? Query { get; set; }
+        public string? Oper {  get; set; }
+        public string MaxResults { get; set; } = "9999999999999999999999";
+
+        public MultipartFormDataContent GetContent()
+        {
+            return new MultipartFormDataContent
+            {
+                { new StringContent($"{ModelName}.{FieldName}"), "qtype" },
+                { new StringContent(Query!), "query" },
+                { new StringContent(Oper!), "oper" },
+                { new StringContent("1"), "page" },
+                { new StringContent(MaxResults), "rp" }
+            };
+        }
+
+        public static QueryBuilder List()
+        {
+            return QueryBuilder.Where("id", ">", "0");
+        }
+
+        public static QueryBuilder Where(string field, string oper, string query)
+        {
+            return new QueryBuilder
+            {
+                FieldName = field,
+                Oper = oper,
+                Query = query
+            };
+        }
+    }
+}
