90da5469216ed295936abe2e071f61a2ecfc6d3b
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-12 17:25:31 -0400

    adicionado a configuração json options

--- IxcNet/Services/Configuration.cs
+++ IxcNet/Services/Configuration.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace IxcNet.Services
 {
     public partial class IxcNetService
     {
+        private static JsonIgnoreCondition JsonIgnoreCOndition;
         HttpClient _http = new();
+        JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+        };
 
-        public IxcNetService() { }
+        public IxcNetService() {}
 
         public IxcNetService(HttpClient http) { }
-
         public void Setup(string host, string token)
         {
             _http.BaseAddress = new Uri(host);
-            _http.DefaultRequestHeaders.Add("ixcsoft", "listar");
             _http.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(token)));
         }
     }
