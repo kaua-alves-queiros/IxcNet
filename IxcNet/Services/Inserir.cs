51bf779168ec616a0aac4c75635a8a824ec61e85
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-10 10:06:17 -0400

    adicionado documentação basica do inserir

--- IxcNet/Services/Inserir.cs
+++ IxcNet/Services/Inserir.cs
@@ -6,6 +6,12 @@ namespace IxcNet.Services
 {
     public partial class IxcNetService
     {
+        /// <summary>
+        /// Insere um novo registro de um determinado modelo na API do IXC.
+        /// </summary>
+        /// <typeparam name="T">O tipo do modelo que deve implementar <see cref="INamedModel"/>.</typeparam>
+        /// <param name="model">O objeto do modelo a ser inserido.</param>
+        /// <returns>O <see cref="HttpStatusCode"/> retornado pela API.</returns>
         public async Task<HttpStatusCode> Inserir<T>(T model) where T : INamedModel
         {
             var route = model.ModelName;
