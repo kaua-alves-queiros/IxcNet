57bacc968a36506464735c0f264450212ac594e7
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-13 19:13:07 -0400

    criad Moq httpHanddler

---
+++ Test/Utils/MockHttpMessageHandler.cs
@@ -0,0 +1,19 @@
+using System.Net;
+
+namespace Test.Utils
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly Func<HttpRequestMessage, HttpResponseMessage> _handler;
+
+        public MockHttpMessageHandler(Func<HttpRequestMessage, HttpResponseMessage> handler)
+        {
+            _handler = handler;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_handler(request));
+        }
+    }
+}
