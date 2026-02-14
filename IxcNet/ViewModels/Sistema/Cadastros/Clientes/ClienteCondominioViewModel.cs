aaa9748acd9a8b2ca763d03a4113747100c74bf4
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-13 23:41:14 -0400

    implementado condominio

---
+++ IxcNet/ViewModels/Sistema/Cadastros/Clientes/ClienteCondominioViewModel.cs
@@ -0,0 +1,29 @@
+﻿using IxcNet.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IxcNet.ViewModels.Sistema.Cadastros.Clientes
+{
+    public class ClienteCondominioViewModel : INamedModel
+    {
+        public string? ModelName => "cliente_condominio";
+
+        public string? id { get; set; }
+        public string? condominio { get; set; }
+        public string? cnpj { get; set; }
+        public string? id_cidade { get; set; }
+        public string? endereco { get; set; }
+        public string? numero { get; set; }
+        public string? cep { get; set; }
+        public string? bairro { get; set; }
+        public string? sindico { get; set; }
+        public string? celular_sindico { get; set; }
+        public string? data_cadastro { get; set; }
+        public string? bloco_unico { get; set; }
+        public string? quantidade_shafts { get; set; }
+        public string? obs { get; set; }
+    }
+}
