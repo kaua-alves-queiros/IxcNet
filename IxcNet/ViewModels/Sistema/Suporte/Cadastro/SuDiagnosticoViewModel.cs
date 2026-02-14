a6e3ee4b5d19b97922adc0c7ac25fc1c7017099b
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-13 23:44:48 -0400

    implementado diagnostico

---
+++ IxcNet/ViewModels/Sistema/Suporte/Cadastro/SuDiagnosticoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using IxcNet.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IxcNet.ViewModels.Sistema.Suporte.Cadastro
+{
+    public class SuDiagnosticoViewModel : INamedModel
+    {
+        public string? ModelName => "su_diagnostico";
+
+        public string? id { get; set; }
+        public string? ativo { get; set; }
+        public string? descricao { get; set; }
+        public string? id_setor { get; set; }
+        public string? ultima_atualizacao { get; set; }
+    }
+}
